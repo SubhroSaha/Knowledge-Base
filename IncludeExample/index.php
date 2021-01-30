bbd46a319019da9efe255d6b47a7c4bba0cd5697
My Name <myemail@example.com>
2021-01-31 02:00:53 +0530

    Added Includes, scopes and globals

---
+++ IncludeExample/index.php
@@ -0,0 +1,24 @@
+    <?php
+        include "header.php";
+    ?>
+    <?php
+        include "banner.php";
+    ?>
+    <?php
+        include "about.php";
+    ?>
+    <?php
+        include "features.php";
+    ?>
+    <?php
+        include "projects.php";
+    ?>
+    <?php
+        include "testimonials.php";
+    ?>
+    <?php
+        include "contact.php";
+    ?>    
+    <?php
+        include "footer.php";
+    ?>
