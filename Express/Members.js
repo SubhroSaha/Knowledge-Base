310653bf1332f2f161b42a08046bd20a05934c53
Subhro Saha <subhro980898@gmail.com>
2021-05-03 12:21:08 +0530

    Added Express

---
+++ Express/Members.js
@@ -0,0 +1,28 @@
+const members = [
+    {
+        id: 1,
+        name: 'Subhro Saha',
+        email: 'demo@email.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Pieu Saha',
+        email: 'pieu@email.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Bipro Saha',
+        email: 'bipro@email.com',
+        status: 'active'
+    },
+    {
+        id: 4,
+        name: 'Shyama Saha',
+        email: 'bonna@email.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
